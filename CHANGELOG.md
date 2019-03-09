2e11b7f5d9480ebe56e05e7461fe7ce8cf39a205
Sam Kingston <sam@sjkwi.com.au>
2019-03-10 07:24:31 +1000

    Version bump

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## 1.0.1 - 2019-03-10
+
+* Fix PyPi long description (@sjkingo)
+* Add support for Python 2 (@sjkingo)
+
+## 1.0.0 - 2019-03-10
+
+* Initial version (@sjkingo)
--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-ebhealthcheck',
-    version='1.0',
+    version='1.0.1',
     license='BSD',
     author='Sam Kingston',
     author_email='sam@sjkwi.com.au',
     description='Django app to add an instance\'s public IP to ALLOWED_HOSTS for Elastic Beanstalk\'s health check system',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/sjkingo/django-ebhealthcheck',
     install_requires=['requests'],
     packages=find_packages(),
